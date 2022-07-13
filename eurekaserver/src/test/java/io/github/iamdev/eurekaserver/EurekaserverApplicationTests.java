1d661d1c42dfd5e2ef888031283d14ea4c1d38db
gicaetano <gisele_sig@hotmail.com>
2022-07-13 11:52:49 -0300

    Conf EurekaServer

---
+++ eurekaserver/src/test/java/io/github/iamdev/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iamdev.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
