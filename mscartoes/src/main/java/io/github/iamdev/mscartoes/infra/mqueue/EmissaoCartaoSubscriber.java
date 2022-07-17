426b041591a792844dea25882c7537127cfec0d7
gicaetano <gisele_sig@hotmail.com>
2022-07-17 15:26:49 -0300

    implemented endpoint publisher/emissaocartao

--- mscartoes/src/main/java/io/github/iamdev/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/io/github/iamdev/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -4,11 +4,40 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.github.iamdev.mscartoes.domain.Cartao;
+import io.github.iamdev.mscartoes.domain.ClienteCartao;
+import io.github.iamdev.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
+import io.github.iamdev.mscartoes.infra.repository.CartaoRepository;
+import io.github.iamdev.mscartoes.infra.repository.ClienteCartaoRepository;
+import lombok.AllArgsConstructor;
+
 @Component
+@AllArgsConstructor
 public class EmissaoCartaoSubscriber {
+	
+	private final CartaoRepository cartaoRepository;
+	private final ClienteCartaoRepository clienteCartaoRepository;
 
 	@RabbitListener(queues = "${mq.queues.emissao-cartoes}")
 	public void receberSolicitacaoEmissao(@Payload String payload) {
-		System.out.println(payload);
+		
+		try {
+			var mapper = new ObjectMapper();
+			DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
+			Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+			ClienteCartao clienteCartao = new ClienteCartao();
+			clienteCartao.setCartao(cartao);
+			clienteCartao.setCpf(dados.getCpf());
+			clienteCartao.setLimite(dados.getLimiteLiberado());
+			
+			clienteCartaoRepository.save(clienteCartao);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
